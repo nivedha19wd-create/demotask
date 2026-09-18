4fbc17f908b2b55db8f6e07c7e72bb3c0c0a6d3e
nivedha19wd-create <nivedha19.wd@gmail.com>
2026-09-18 14:41:09 +0530

    update

--- ecommerce.ts
+++ ecommerce.ts
@@ -1,134 +1,91 @@
-
-interface Customer {
-    customerName: string;
-    customerId: string;
-    membershipType: MembershipType;}
-interface Product {productName: string;
-    productCategory: string;
-    productPrice: number;
-    quantityPurchased: number;
-    productStock: number;}
-const customer: Customer = {
-    customerName: "nivedha",
-    customerId: "12345",
-    membershipType: "Gold"
+let customerName: string = "nivedha";
+let customerId: number = 1234;
+let membershipType: string = "Gold";
+let product = {productName: "dress",productCategory: "fashion",productPrice: 4000,quantityPurchased: 1,
+stockAvailable: 5
 };
-type MembershipType = "Silver" | "Gold" | "Platinum" | "Regular";
-const products: Product[] = [
-    {
-        productName: "dress1",
-        productCategory: "fashion",
-        productPrice: 3000,
-        quantityPurchased: 1,
-        productStock: 3
-    },
-    {
-        productName: "dress2",
-        productCategory: "fashion",
-        productPrice: 1500,
-        quantityPurchased: 2,
-        productStock: 5
-    }
-];
-const enteredCoupon: string = "cashback100";
-const validCoupon: string = "cashback100";
-const couponDiscountRate: number = 0.100;
-const gstRate: number = 0.18;
-const DeliveryCharge: number = 100;
-let membershipDiscountRate: number;
-switch (customer.membershipType) {
-    case "Platinum":
-        membershipDiscountRate = 0.15;
-        break;
+let subtotal = 0;
+let membershipDiscount = 0;
+let couponDiscount = 0;
+let gstAmount = 0;
+let deliveryCharge = 0;
+let finalBill = 0;
+let enteredCoupon: string = "CASHBACK10";
+let validCoupon: string = "CASHBACK10";
+let couponDiscountRate = 10;
+let orderStatus: string;
 
-    case "Gold":
-        membershipDiscountRate = 0.10;
-        break;
 
-    case "Silver":
-        membershipDiscountRate = 0.5;
-        break;
-
-    case "Regular":
-        membershipDiscountRate = 0;
-        break;
-
-    default:
-        membershipDiscountRate = 0;
+if (product.stockAvailable > 0) {
+orderStatus = "in stock";
+subtotal = product.productPrice * product.quantityPurchased;
+if (membershipType === "Platinum") {
+membershipDiscount = subtotal * 20 / 100;
 }
-let subtotal: number = 0;
-let orderStatus: string = "in progress";
-for (const product of products) 
-    {
-if (product.productStock > 0)
-     {
- if (product.quantityPurchased <= product.productStock) 
-    {
-            subtotal = product.productPrice * product.quantityPurchased;
-        } else 
-            {
-        orderStatus = `In stock for ${product.productName}`;
-        }
-
-    } else 
-        {
-        orderStatus = `Out of Stock: ${product.productName}`;
-    }
+else if (membershipType === "Gold") {
+membershipDiscount = subtotal * 15 / 100;
 }
-
-
-const membershipDiscount: number =subtotal *membershipDiscountRate;
-let couponDiscount: number = 0;
-let couponStatus: string;
-if (enteredCoupon === validCoupon) 
-    {
-    couponDiscount = (subtotal - membershipDiscount) *couponDiscountRate;
-    couponStatus = "Coupon Applied";
-} else {
-    couponDiscount = 0;
-    couponStatus = "Invalid Coupon ";
+else if (membershipType === "Silver") {
+membershipDiscount = subtotal * 10 / 100;
 }
-let discountedAmount: number =
-    subtotal - membershipDiscount- couponDiscount;
-const gstAmount: number =
-    discountedAmount * gstRate;
-let finalBillAmount: number =
-    discountedAmount + gstAmount;
-const deliveryCharge: number =
-    finalBillAmount >= 2000? 0:DeliveryCharge;
-
-const deliveryStatus: string =
-    finalBillAmount >= 2000? "Free Delivery": "100";
-finalBillAmount + deliveryCharge;
-const customerIdValue: string =
-    customer.customerId as string;
-
-let productDetails = "";
-
-for (const product of products) {
-    productDetails += `
-Product Name     : ${product.productName}
-Category         : ${product.productCategory}
-Price            : ₹${product.productPrice.toFixed(2)}
-Quantity         : ${product.quantityPurchased}
-Stock Available  : ${product.productStock}`;
+else {
+membershipDiscount = 0;
 }
-
-const invoice = `
-CUSTOMER DETAILS:
-Customer Name     : ${customer.customerName}
-Customer ID     : ${customerIdValue}
-Membership Type  : ${customer.membershipType}
-PRODUCT DETAILS ${productDetails}
+let discount : number;
+switch (membershipType) 
+{
+case "Platinum":
+discount = 20;
+break;
+case "Gold":
+discount = 15;
+break;
+case "Silver":
+discount = 10;
+break;
+default:
+discount = 0;
+}
+if (product.stockAvailable > 0) {
+if (enteredCoupon === validCoupon) {
+couponDiscount = subtotal * couponDiscountRate / 100;
+}
+else {
+couponDiscount = 0;
+}}
+let billamount = subtotal - membershipDiscount - couponDiscount;
+gstAmount = billamount * 18 / 100;
+finalBill = billamount + gstAmount;
+deliveryCharge = finalBill >= 2000 ? 0 : 100;
+finalBill += deliveryCharge;
+product.stockAvailable -= product.quantityPurchased;
+}
+else {
+orderStatus = "Out of Stock";
+}
+let invoice = `INVOICE
+CUSTOMER DETAILS
+Customer Name       : ${customerName}
+Customer ID         : ${customerId}
+Membership Type     : ${membershipType}
+PRODUCT DETAILS
+Product Name        : ${product.productName}
+Category            : ${product.productCategory}
+Price               : ₹${product.productPrice}
+Quantity            : ${product.quantityPurchased}
+Stock Available     : ${product.stockAvailable}
 BILL DETAILS
-Subtotal                    : ₹${subtotal}
-Membership Discount      : ₹${membershipDiscount}
-Coupon Discount             : ₹${couponDiscount}
-Coupon Status            : ${couponStatus}
-GST (${gstRate }%)              : ₹${gstAmount}
-Delivery Charge            : ₹${deliveryCharge}
-FINAL AMOUNT    : ₹${finalBillAmount}
-ORDER STATUS            : ${orderStatus}
+Subtotal            : ₹${subtotal.toFixed(2)}
+Membership Discount : ₹${membershipDiscount.toFixed(2)}
+Coupon Discount     : ₹${couponDiscount.toFixed(2)}
+GST (18%)           : ₹${gstAmount.toFixed(2)}
+Delivery Charge     : ₹${deliveryCharge.toFixed(2)}
+Final bill amount       : ₹${finalBill.toFixed(2)}
+Coupon Applied      : ${couponDiscount }
+Delivery Status     : ${deliveryCharge === 0 ? "FREE DELIVERY" : "₹100 Delivery Charge"}
+Order Status        : ${orderStatus}
+              THANK YOU FOR SHOPPING!
+
 `;
 
 console.log(invoice);
---
+++ testtask.ts
@@ -0,0 +1,13 @@
+/* function greet(a:string,b:number):string
+{
+    return a+b;}
+    console.log("name and age is :",greet("nivedha",30)); */
+
+//anonymous function
+const greet= function(name:string):string
+{
+    return "hello  " + name ;
+};
+console.log(greet("nivedha"));
+console.log(greet("sita"));
+console.log(greet("ram"));
